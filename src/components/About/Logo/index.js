1bea35d42d1f72858f7f0bfadd8e50d730a1e637
ducnguyen10214 <74991701+ducnguyen10214@users.noreply.github.com>
2022-05-19 08:51:23 -0700

    attempt to fix skills 2

--- src/components/About/Logo/index.js
+++ src/components/About/Logo/index.js
@@ -4,7 +4,7 @@ import Portrait from '../../../assets/images/self2.png'
 const Logo = () => {
   return (
     <div className='logo-container'>
-      <img className='logo' src={Portrait} alt='P'/>
+      <img className='logo' src={Portrait} width="900" height="1200" alt='P'/>
     </div>
   )
 }
